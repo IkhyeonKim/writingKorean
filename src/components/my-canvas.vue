8a7194f85950c93b58885700435dfba626c6139e
Ikhyeon <kih199212@gmail.com>
2020-05-20 19:56:52 +0900

    Add backgroun on canvas

--- src/components/my-canvas.vue
+++ src/components/my-canvas.vue
@@ -1,3 +1,4 @@
+
 <template>
     <canvas @mousedown="startPainting" 
 			@mouseup="finishedPainting" 
@@ -6,16 +7,18 @@
 </template>
 
 <script>
+// import Background from '../assets/'
 export default {
 	// Clear
-	// Erase
+    // Erase
+    
     data() {
         return {
             message: "Hello world!",
             canvas: null,
             ctx: null,
             painting: false,
-            vueCanvas: null
+            vueCanvas: null,
         };
     },
 
@@ -43,8 +46,18 @@ export default {
 
             this.ctx.beginPath();
             this.ctx.moveTo(e.clientX, e.clientY - this.canvas.offsetTop);
+        },
+
+        importAll(r) {
+            return r.keys().map(r);
         }
     },
+
+    // beforeMount(){
+    //     const images = this.importAll(require.context('../assets', false, /\.(png|jpe?g|svg)$/));
+    //     console.log('beforeMount')
+    //     console.log(images)
+    // },
 
     mounted() {
         this.canvas = document.querySelector("#canvas");
@@ -52,6 +65,20 @@ export default {
 
         this.canvas.height = 400;
         this.canvas.width = 1000;
+
+        const images = this.importAll(require.context('../assets', false, /\.(png|jpe?g|svg)$/));
+        
+        console.log(images[0].default)
+
+        // console.log(Background)
+        const background = new Image()
+        background.src = images[0].default
+
+        background.onload = () => {
+            this.ctx.drawImage(background, 0 ,0)
+        }
+
+        this.ctx.drawImage(background, 0, 0);
 
         this.vueCanvas = this.ctx;
     }
